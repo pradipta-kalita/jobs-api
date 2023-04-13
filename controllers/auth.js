76047931dba0d1e8d46af1d5856857ef908cb2a9
pradipta-kalita <contact.pradiptakalita@gmail.com>
2023-04-13 15:05:53 +0530

    Apr 12

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,7 +13,58 @@ exports.register = async (req, res) => {
     token,
   });
 };
+exports.signup = (req, res) => {
+  res.send(`
+  <!DOCTYPE html>
+<html>
+  <head>
+    <title>Sign Up</title>
+    <style>
+      /* Add some styles to the form elements */
+      input[type="email"],
+      input[type="password"],
+      input[type="text"] {
+        display: block;
+        margin: 10px 0;
+        padding: 10px;
+        width: 100%;
+        border-radius: 5px;
+        border: 1px solid #ccc;
+        font-size: 16px;
+      }
+      input[type="submit"] {
+        background-color: #4caf50;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        padding: 10px 20px;
+        font-size: 16px;
+        cursor: pointer;
+      }
+      input[type="submit"]:hover {
+        background-color: #3e8e41;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Sign Up</h1>
+    <form method="POST" action="/signup">
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" required />
 
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required />
+
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" required />
+
+      <input type="submit" value="Sign Up" />
+    </form>
+  </body>
+</html>
+
+  `);
+};
 exports.login = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../controllers/auth');
-
+router.get('/register', authController.signup);
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
